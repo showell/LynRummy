fac89803535245a45959b6f76be6a6419ba03b5a
Steve Howell <showell30@yahoo.com>
2026-01-18 06:09:11 -0500

    wip

--- card.ts
+++ card.ts
@@ -1,35 +1,79 @@
 const enum CardValue {
-    ACE = "A",
-    TWO = "2",
-    THREE = "3",
-    FOUR = "4",
-    FIVE = "5",
-    SIX = "6",
-    SEVEN = "7",
-    EIGHT = "8",
-    NINE = "9",
-    TEN = "10",
-    JACK = "J",
-    QUEEN = "Q",
-    KING = "K",
-};
+    ACE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    TEN = 10,
+    JACK = 11,
+    QUEEN = 12,
+    KING = 13,
+}
 
-function successor(val: CardValue): CardValue {
+function str(val: CardValue): string {
     switch (val) {
-        case CardValue.ACE: return CardValue.TWO;
-        case CardValue.TWO: return CardValue.THREE;
-        case CardValue.THREE: return CardValue.FOUR;
-        case CardValue.FOUR: return CardValue.FIVE;
-        case CardValue.FIVE: return CardValue.SIX;
-        case CardValue.SIX: return CardValue.SEVEN;
-        case CardValue.SEVEN: return CardValue.EIGHT;
-        case CardValue.EIGHT: return CardValue.NINE;
-        case CardValue.NINE: return CardValue.TEN;
-        case CardValue.TEN: return CardValue.JACK;
-        case CardValue.JACK: return CardValue.QUEEN;
-        case CardValue.QUEEN: return CardValue.KING;
-        case CardValue.KING: return CardValue.ACE;
+        case CardValue.ACE:
+            return "A";
+        case CardValue.TWO:
+            return "2";
+        case CardValue.THREE:
+            return "3";
+        case CardValue.FOUR:
+            return "4";
+        case CardValue.FIVE:
+            return "5";
+        case CardValue.SIX:
+            return "6";
+        case CardValue.SEVEN:
+            return "7";
+        case CardValue.EIGHT:
+            return "8";
+        case CardValue.NINE:
+            return "9";
+        case CardValue.TEN:
+            return "10";
+        case CardValue.JACK:
+            return "J";
+        case CardValue.QUEEN:
+            return "Q";
+        case CardValue.KING:
+            return "K";
     }
 }
 
-console.log(successor(CardValue.SEVEN));
+function successor(val: CardValue): CardValue {
+    switch (val) {
+        case CardValue.ACE:
+            return CardValue.TWO;
+        case CardValue.TWO:
+            return CardValue.THREE;
+        case CardValue.THREE:
+            return CardValue.FOUR;
+        case CardValue.FOUR:
+            return CardValue.FIVE;
+        case CardValue.FIVE:
+            return CardValue.SIX;
+        case CardValue.SIX:
+            return CardValue.SEVEN;
+        case CardValue.SEVEN:
+            return CardValue.EIGHT;
+        case CardValue.EIGHT:
+            return CardValue.NINE;
+        case CardValue.NINE:
+            return CardValue.TEN;
+        case CardValue.TEN:
+            return CardValue.JACK;
+        case CardValue.JACK:
+            return CardValue.QUEEN;
+        case CardValue.QUEEN:
+            return CardValue.KING;
+        case CardValue.KING:
+            return CardValue.ACE;
+    }
+}
+
+console.log(str(successor(CardValue.SEVEN)));
